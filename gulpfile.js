b813250707b793938e8ef6cbae6990d114be9d33
Andrew Nichols <tandrewnichols@gmail.com>
2016-12-29 15:41:30 -0500

    Switch to eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-var gulp = require('gulp');
-var sequence = require('gulp-sequence');
-require('file-manifest').generate('./gulp', ['*.js', '!config.js']);
+const gulp = require('gulp');
+const sequence = require('gulp-sequence');
+require('file-manifest').generate('./gulp', { match: ['*.js', '!config.js'] });
 gulp.task('travis', sequence(['default'], 'codeclimate'));
 gulp.task('test', sequence(['clean'], ['cover']));
 gulp.task('default', sequence(['lint', 'clean'], ['cover']));
